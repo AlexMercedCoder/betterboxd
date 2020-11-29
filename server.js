92cf349703d43668407387da558b9f07a06bc632
Alex Merced <willcodeforfood@alexmerced.com>
2020-11-29 13:37:21 -0500

    session store update

--- server.js
+++ server.js
@@ -1,51 +1,56 @@
 // ========================
 // DEPENDENCIES
 // ========================
-const express = require('express')
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+require("dotenv").config();
 
 // controllers =============
-const moviesControl = require('./controllers/moviesController.js');
-const usersControl = require('./controllers/usersController.js');
-const sessionControl = require('./controllers/sessionController.js');
-
+const moviesControl = require("./controllers/moviesController.js");
+const usersControl = require("./controllers/usersController.js");
+const sessionControl = require("./controllers/sessionController.js");
 
 // ========================
 // GLOBAL CONFIG
 // ========================
 const app = express();
 const db = mongoose.connection;
-const host = process.env.CLUSTER
-const port = process.env.PORT || 5000
-const dbupdateobject = { useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify:false , useCreateIndex: true};
+const host = process.env.CLUSTER;
+const port = process.env.PORT || 5000;
+const dbupdateobject = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
 const mongoURI = host;
 
 // ========================
 // MIDDLEWARE
 // ========================
 
-app.use(session({
+app.use(
+  session({
     secret: "iamlikeabird", //some random string
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 app.use(express.json());
 
-
 //Static Files ==============
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Controllers ==============
 
-app.use('/moviesapi', moviesControl);
-app.use('/users', usersControl);
-app.use('/sessions', sessionControl);
-
-
+app.use("/moviesapi", moviesControl);
+app.use("/users", usersControl);
+app.use("/sessions", sessionControl);
 
 // // ========================
 // // TEST ROUTE
@@ -54,7 +59,6 @@ app.use('/sessions', sessionControl);
 //   res.send('hello world')
 // })
 
-
 // ========================
 // CONNECTION
 // ========================
@@ -62,23 +66,19 @@ app.use('/sessions', sessionControl);
 //DATABASE ==============
 
 // Connect to Mongo
-mongoose.connect( mongoURI, dbupdateobject );
+mongoose.connect(mongoURI, dbupdateobject);
 
 // Connection Error/Success
-db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
-db.on('connected', () => console.log('mongo connected: ', mongoURI));
-db.on('disconnected', () => console.log('mongo disconnected'));
-db.on( 'open' , ()=>{
-  console.log('Connection made!');
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", mongoURI));
+db.on("disconnected", () => console.log("mongo disconnected"));
+db.on("open", () => {
+  console.log("Connection made!");
 });
-
 
 /////////////////////////
 //RUNTIME DATA
 /////////////////////////
 
-
-
-
 //LISTENER ==============
-app.listen(port, () => console.log(`listening on ${port}!`))
+app.listen(port, () => console.log(`listening on ${port}!`));
